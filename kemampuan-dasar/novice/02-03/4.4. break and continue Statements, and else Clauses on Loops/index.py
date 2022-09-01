b0756079103add8b67d431b80f292b6d3588e9b3
novanazmi14501 <novanazmi145@gmail.com>
2022-09-01 12:01:22 +0700

    materi html

---
+++ kemampuan-dasar/novice/02-03/4.4. break and continue Statements, and else Clauses on Loops/index.py
@@ -0,0 +1,31 @@
+for n in range(2, 10):
+for x in range(2, n):
+if n % x == 0:
+ print(n, 'equals', x, '*', n//x)
+ break
+else:
+# loop fell through without finding a factor
+print(n, 'is a prime number')
+
+2 is a prime number
+3 is a prime number
+4 equals 2 * 2
+5 is a prime number
+6 equals 2 * 3
+7 is a prime number
+8 equals 2 * 4
+9 equals 3 * 3
+for num in range(2, 10):
+if num % 2 == 0:
+print("Found an even number", num)
+continue
+print("Found an odd number", num)
+
+Found an even number 2
+Found an odd number 3
+Found an even number 4
+Found an odd number 5
+Found an even number 6
+Found an odd number 7
+Found an even number 8
+Found an odd number 9
